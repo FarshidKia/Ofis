5da16c3e395e836838c70c5426d144e10ef2a5dd
FarshidKia <ninafarshidkia@gmail.com>
2019-12-16 17:30:16 +0300

    Personel CRUD işlemleri

---
+++ Controllers/PersonelController.cs
@@ -0,0 +1,20 @@
+﻿using Ofis_ISE309.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ofis_ISE309.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        OfisEntities1 db = new OfisEntities1();
+        public ActionResult Index()
+        {
+            var model = db.Personel.ToList();
+            return View(model);
+        }
+    }
+}
