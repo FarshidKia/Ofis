0c922cfad6f4a35f4eb8a1afe4afb64c8ebd8ec4
FarshidKia <ninafarshidkia@gmail.com>
2019-12-17 22:42:00 +0300

    Ajax ile Silme işlemi gerçekleştirildi.

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,4 +1,30 @@
-﻿function CheckDateTypeIsValid(dateElement)
+﻿$(function () {
+    $("#tblDepartmanlar").on("click", ".btnDepartmanSil", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinizden eöinmisiniz?", function (result) {
+            if (result) {
+                var id = btn.data("id");
+                
+                $.ajax({
+                    type: "GET";
+                    URL: "Departman/Sil/" + id,
+                    success: function () {
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        })
+
+        
+        
+    });
+});
+
+
+function CheckDateTypeIsValid(dateElement)
 {
     var value = $(dateElement).val();
     if (value == "") {
